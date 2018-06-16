2f9fea3274375f3b543458dd6ca149414194aa93
dgnsrekt <run2devtest@gmail.com>
2018-06-16 07:11:29 -0500

    mvd states.toml path out of config folder.

--- fomo_gunbot_plus/constants.py
+++ fomo_gunbot_plus/constants.py
@@ -8,11 +8,11 @@ assert CONFIGURATION_PATH.exists(), f'{CONFIGURATION_PATH} file missing.'
 CLEAN_JSON_CONFIG_PATH = BASEPATH.parent / 'clean.json'
 assert CLEAN_JSON_CONFIG_PATH.exists(), f'{CLEAN_JSON_CONFIG_PATH} file missing.'
 
-STATE_PATH = CONFIGURATION_PATH / 'states.toml'  # TODO: Change to toml
+STATE_PATH = BASEPATH.parent / 'states.toml'  # TODO: Change to toml
 assert STATE_PATH.exists(), f'{STATE_PATH} file missing.'
 
 GUNBOT_PATH = BASEPATH.parent / 'gunbot'
-assert GUNBOT_PATH.exists(), f'{STATE_PATH} file missing.'
+assert GUNBOT_PATH.exists(), f'{STATE_PATH} file missing.'  # TODO addmessage may need download gb
 
 CONFIG_JS_PATH = GUNBOT_PATH / 'config.js'
 assert CONFIG_JS_PATH.exists(), f'{STATE_PATH} file missing.'
