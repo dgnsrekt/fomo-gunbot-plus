57e54e452ea46e4aabd0323be2294e459c15620c
dgnsrekt <run2devtest@gmail.com>
2018-06-17 12:26:48 -0500

    removed assert checks

--- fomo_gunbot_plus/constants.py
+++ fomo_gunbot_plus/constants.py
@@ -2,17 +2,18 @@ from pathlib import Path
 
 BASEPATH = Path(__file__).parent
 
-CONFIGURATION_PATH = BASEPATH.parent / 'configuration'
-assert CONFIGURATION_PATH.exists(), f'{CONFIGURATION_PATH} file missing.'
-
 CLEAN_JSON_CONFIG_PATH = BASEPATH.parent / 'clean.json'
-assert CLEAN_JSON_CONFIG_PATH.exists(), f'{CLEAN_JSON_CONFIG_PATH} file missing.'
 
-STATE_PATH = BASEPATH.parent / 'states.toml'  # TODO: Change to toml
-assert STATE_PATH.exists(), f'{STATE_PATH} file missing.'
+CONFIGURATION_PATH = BASEPATH.parent / 'configuration'
+
+STATE_PATH = BASEPATH.parent / 'states.toml'
+
+DELISTED_PATH = BASEPATH.parent / 'delisted.toml'
 
 GUNBOT_PATH = BASEPATH.parent / 'gunbot'
-assert GUNBOT_PATH.exists(), f'{STATE_PATH} file missing.'  # TODO addmessage may need download gb
 
 CONFIG_JS_PATH = GUNBOT_PATH / 'config.js'
-assert CONFIG_JS_PATH.exists(), f'{STATE_PATH} file missing.'
+
+GUNBOT_DOWNLOAD_PATH = BASEPATH.parent / 'lin.zip'
+
+TEMP_GUNBOT_EXTRACTPATH = BASEPATH.parent / 'gunbot_temp'
