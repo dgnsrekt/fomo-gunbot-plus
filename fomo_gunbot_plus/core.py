af4b215ec899b50ef75559fa7f17a9a9a0885df8
dgnsrekt <run2devtest@gmail.com>
2018-06-20 19:02:40 -0500

    added hot/cold filters

--- fomo_gunbot_plus/core.py
+++ fomo_gunbot_plus/core.py
@@ -1,14 +1,50 @@
-from .gunbot_interface import GunBotConfigInterface
+from .gunbot_interface import GunBotConfigInterface, GunBotStateInterface
 
 from fomo_superfilter.interface import BinanceDataFrameCreator
 from fomo_superfilter.superfilter import SuperFilter
+
 from .models.superhot import SuperHot, create_superhot_table
+from .models.balance import Balance, create_balance_table
 from time import sleep
 
 
 def display_equity_curve_url():  # IDEA: Move to chart module
     print('Equity Curve Running on...')
     print('* Running on http://127.0.0.1:8050/ (Press CTRL+C to quit)')
+
+
+def filter_hot():
+    print('Searching for hot coins...')
+    binance_data = BinanceDataFrameCreator.prepare_dataframes()
+    binance_btc = binance_data['BTC']
+    binance_hot = SuperFilter.filter(binance_btc)
+    binance_hot = [hot.split('BTC')[0] for hot in binance_hot]
+
+    binance_names = list(binance_btc.index)
+    binance_names = [name.split('BTC')[0] for name in binance_names]
+    binance_names.sort()
+
+    if not SuperHot.table_exists():
+        create_superhot_table()
+
+    for hot in binance_hot:
+        SuperHot.add_coin(hot)
+        print(f'Superfilter found {hot}.')
+
+
+def filter_cold():
+    print('Fetching bags...')
+    gbot_pipe = GunBotStateInterface()
+
+    bags = gbot_pipe.fetch_bags
+    all_pairs = gbot_pipe.all_pairs
+
+    if not Balance.table_exists():
+        create_balance_table()
+
+    Balance.add(gbot_pipe.estimated_value)
+
+    return bags, all_pairs
 
 
 def run():
@@ -18,29 +54,16 @@ def run():
     GBI.write_to_gunbot_config()
 
     while True:
-        print('Searching for hot coins...')
+        filter_hot()
+        bags, all_pairs = filter_cold()
+        superhot = SuperHot.fetch_hot()
 
-        binance_data = BinanceDataFrameCreator.prepare_dataframes()
-        binance_btc = binance_data['BTC']
-        binance_hot = SuperFilter.filter(binance_btc)
-        binance_hot = [hot.split('BTC')[0] for hot in binance_hot]
+        print(f'bags: {bags}')
+        print(f'pairs: {all_pairs}')
+        print(f'Current hot coins in db:{superhot}')
 
-        binance_names = list(binance_btc.index)
-        binance_names = [name.split('BTC')[0] for name in binance_names]
-        binance_names.sort()
-
-        if not SuperHot.table_exists():
-            create_superhot_table()
-
-        for hot in binance_hot:
-            SuperHot.add_coin(hot)
-            print(f'Superfilter found {hot}.')
-
-        hot_current = SuperHot.fetch_hot()
         # others_current = [other for other in binance_names if other not in hot_current]
         # print(len(binance_names))
         # print(len(others_current))
-
-        print(f'Current hot coins in db:{hot_current}')
 
         sleep(30)
