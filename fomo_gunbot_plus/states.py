ad81ab474cb60858a8cbc3ceb1b2295cc1263ed4
dgnsrekt <run2devtest@gmail.com>
2018-06-23 09:07:55 -0500

    refactored states no need for transitions

--- fomo_gunbot_plus/states.py
+++ fomo_gunbot_plus/states.py
@@ -8,16 +8,6 @@ class State(ABC):
     Class to define a state object.
     """
 
-    def __init__(self, coin):
-        print(f'{coin} state:', str(self))
-
-    @abstractmethod
-    def on_event(self, event):
-        """
-        Handle events that are delegated to this State.
-        """
-        pass
-
     def __repr__(self):
         """
         Leverages the __str__ method to describe the State.
@@ -31,107 +21,33 @@ class State(ABC):
         return self.__class__.__name__
 
 
-FROZEN = []
-COLD = []
-HOT = []
-
-
-class FrozenState(State):
-    def __init__(self, coin):
-        super().__init__(coin)
-        self.coin = coin
-        FROZEN.append(self.coin)
-
-    def on_event(self, state):
-        if state.lower() == 'cold':
-            return self.transition_frozen_to_cold()
-        elif state.lower() == 'hot':
-            return self.transition_frozen_to_hot()
-        else:
-            return FrozenState(self.coin)
-
-    def transition_frozen_to_hot(self):
-        print(f'{self.coin} transtitioning frozen to hot.')
-        FROZEN.remove(self.coin)
-        return HotState(self.coin)
-
-    def transition_frozen_to_cold(self):
-        print(f'{self.coin} transtitioning frozen to cold.')
-        FROZEN.remove(self.coin)
-        return ColdState(self.coin)
-
-
 class ColdState(State):
-    def __init__(self, coin):
-        super().__init__(coin)
-        self.coin = coin
-        COLD.append(self.coin)
+    def __init__(self, coins):
+        self.coins = coins
+        self.enabled = True
+        self.pair_configuration = dict()
+        self.pair_configuration['strategy'] = 'emotionless'
+        self.pair_configuration['override'] = {'BUY_ENABLED': False, 'GAIN': 0.25}
 
-    def on_event(self, state):
-        if state.lower() == 'hot':
-            return self.transition_cold_to_hot()
-        if state.lower() == 'frozen':
-            return self.transition_cold_to_frozen()
-        else:
-            return ColdState(self.coin)
-
-    def transition_cold_to_hot(self):
-        print(f'{self.coin} transtitioning cold to hot.')
-        COLD.remove(self.coin)
-        return HotState(self.coin)
-
-    def transition_cold_to_frozen(self):
-        print(f'{self.coin} transtitioning cold to frozen.')
-        COLD.remove(self.coin)
-        return FrozenState(self.coin)
+    def prepare_config(self):
+        config = dict()
+        for coin in self.coins:
+            name = f'BTC-{coin}'.upper()
+            config[name] = self.pair_configuration
+        return config
 
 
 class HotState(State):
-    def __init__(self, coin):
-        super().__init__(coin)
-        self.coin = coin
-        HOT.append(coin)
+    def __init__(self, coins):
+        self.coins = coins
+        self.enabled = True
+        self.pair_configuration = dict()
+        self.pair_configuration['strategy'] = 'emotionless'
+        self.pair_configuration['override'] = {}
 
-    def on_event(self, state):
-        if state.lower() == 'cold':
-            return self.transition_hot_to_cold()
-        elif state.lower() == 'frozen':
-            return self.transition_hot_to_frozen()
-        else:
-            return HotState(self.coin)
-
-    def transition_hot_to_cold(self):
-        print(f'{self.coin} transtitioning hot to cold.')
-        HOT.remove(self.coin)
-        return ColdState(self.coin)
-
-    def transition_hot_to_frozen(self):
-        print(f'{self.coin} transtitioning hot to frozen.')
-        HOT.remove(self.coin)
-        return FrozenState(self.coin)
-
-
-# FROZEN_FILE = ['BCH', 'BCC']
-# COLD_FILE = ['BTC', 'ETC', 'ETH']
-# HOT_FILE = ['BKCAT', 'NANO']
-#
-# coin_states = dict()
-#
-# for f in FROZEN_FILE:
-#     coin_states[f] = FrozenState(f)
-#
-# for c in COLD_FILE:
-#     coin_states[c] = ColdState(c)
-#
-# for h in HOT_FILE:
-#     coin_states[h] = HotState(h)
-#
-#
-# while True:
-#     coin = choice(list(coin_states.keys()))
-#     state = choice(['hot', 'cold', 'frozen'])
-#     print()
-#     print('RANDOM', coin, 'Old-State:', coin_states[coin], 'New-State:', state)
-#     print('=' * 25)
-#     coin_states[coin] = coin_states[coin].on_event(state)
-#     sleep(1)
+    def prepare_config(self):
+        config = dict()
+        for coin in self.coins:
+            name = f'BTC-{coin}'.upper()
+            config[name] = self.pair_configuration
+        return config
